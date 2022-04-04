16632599b55257f6eb5953854f7519036d6e06bd
EHGray <101975885+EHGray@users.noreply.github.com>
2022-04-04 15:30:56 +0100

    Update flask.py

--- flask.py
+++ flask.py
@@ -7,4 +7,6 @@ def search_page():
     html = '<div class="container-out">\n<div class="container-in">\n<div class="search-container">\n<div class="search-engine">\n<p class="search-title">Search Names\n</p>\n<input \ntype="input"\n id="search-input"\n autocomplete="off"\nplaceholder="Hit Enter to Search"\n/>\n</div>\n<div id="search-results"></div>\n<div id="search-data"></div>\n</div>\n</div>\n</div>'
     return html
 
+if __name__ == "__main__":
+    app.run(ssl_context='adhoc')
     
