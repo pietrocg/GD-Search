3e1c31fa248d47ac0d3cdaaf2c94e6462cd75782
EHGray <101975885+EHGray@users.noreply.github.com>
2022-04-04 15:30:12 +0100

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def search_page():
     html = '<div class="container-out">\n<div class="container-in">\n<div class="search-container">\n<div class="search-engine">\n<p class="search-title">Search Names\n</p>\n<input \ntype="input"\n id="search-input"\n autocomplete="off"\nplaceholder="Hit Enter to Search"\n/>\n</div>\n<div id="search-results"></div>\n<div id="search-data"></div>\n</div>\n</div>\n</div>'
     return html
+
+    
